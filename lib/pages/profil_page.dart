1c8943f3856fc3b0236fd0004fbd4890b9ee5046
OnArap <onarap95@gmail.com>
2023-09-05 12:04:46 +0200

    Add profil image, name and situation in ProfilPage

--- lib/pages/profil_page.dart
+++ lib/pages/profil_page.dart
@@ -12,8 +12,37 @@ class ProfilPage extends StatefulWidget {
 class _ProfilPageState extends State<ProfilPage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(),
+    return SafeArea(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 10.0),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Theme.of(context).colorScheme.secondary,
+                  border: Border.all(color: Theme.of(context).colorScheme.primary, width: 5.0, style: BorderStyle.solid),
+                  image: DecorationImage(fit: BoxFit.contain, image: Image.asset('assets/profil.jpg').image),
+                ),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                'Vincent Burel',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.primary),
+              ),
+              const SizedBox(height: 5.0),
+              Text(
+                'Flutter enthusiast',
+                style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.primary),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
