b2cae8029031e3144edda975b8389b3a26f14452
OnArap <onarap95@gmail.com>
2023-09-13 15:51:03 +0200

    Icons information updated + ListTile into Row

--- lib/constant/informations.dart
+++ lib/constant/informations.dart
@@ -22,18 +22,21 @@ class InformationStates extends State<Informations> {
               borderRadius: const BorderRadius.all(Radius.circular(5)),
               border: Border.all(color: Colors.grey, width: 2.0, style: BorderStyle.solid),
             ),
-            child: GridView.count(
-              crossAxisCount: 2,
-              childAspectRatio: (1 / .3),
-              shrinkWrap: true,
-              children: const [
-                ListTile(leading: Text("age"), title: Text("32", style: TextStyle(fontSize: 12))),
-                ListTile(leading: Text("gender"), title: Text("M")),
-                ListTile(leading: Text("nationality"), title: Text("FR")),
-                ListTile(leading: Text("email"), title: Text("onarap95@gmail.com", style: TextStyle(fontSize: 12))),
-                ListTile(leading: Text("phone"), title: Text("0674707763", style: TextStyle(fontSize: 12))),
-                ListTile(leading: Text("spoken languages"), title: Text("FR, EN")),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: (1 / .3),
+                shrinkWrap: true,
+                children: const [
+                  Row(children: [Icon(Icons.cake), SizedBox(width: 10), Expanded(child: Text("32", style: TextStyle(fontSize: 12)))]),
+                  Row(children: [Icon(Icons.person_pin_sharp), SizedBox(width: 10), Expanded(child: Text("M", style: TextStyle(fontSize: 12)))]),
+                  Row(children: [Icon(Icons.outlined_flag), SizedBox(width: 10), Expanded(child: Text("FR", style: TextStyle(fontSize: 12)))]),
+                  Row(children: [Icon(Icons.email_outlined), SizedBox(width: 10), Expanded(child: Text("onarap95@gmail.com", style: TextStyle(fontSize: 12)))]),
+                  Row(children: [Icon(Icons.contact_phone_outlined), SizedBox(width: 10), Expanded(child: Text("0674707763", style: TextStyle(fontSize: 12)))]),
+                  Row(children: [Icon(Icons.language_outlined), SizedBox(width: 10), Expanded(child: Text("FR, EN", style: TextStyle(fontSize: 12)))]),
+                ],
+              ),
             ),
           ),
         ],
