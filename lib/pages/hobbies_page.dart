13166ce15f911d4212d6935e4dc39734d9bf71d0
OnArap <onarap95@gmail.com>
2023-09-26 16:23:20 +0200

    first commit for Hobbies Pages

--- lib/pages/hobbies_page.dart
+++ lib/pages/hobbies_page.dart
@@ -3,7 +3,10 @@ import '../constants/destinations.dart';
 
 class HobbiesPage extends StatefulWidget {
   final Destination destination;
-  const HobbiesPage({super.key, required this.destination});
+  final List emojis = ["🥗", "🍻", "🍔", "🏐", "🚲", "🎮", "♟", "🎨", "☕️", "🌳"];
+  final List hobbies = ["veggie food", "party/afterwork", "street food", "volleyball", "fixe-gear bikes", "video games", "chess", "tattoo", "coffee", "ecology"];
+
+  HobbiesPage({super.key, required this.destination});
 
   @override
   State<HobbiesPage> createState() => _HobbiesPageState();
@@ -12,14 +15,41 @@ class HobbiesPage extends StatefulWidget {
 class _HobbiesPageState extends State<HobbiesPage> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hobbies'),
-            CircularProgressIndicator(color: Colors.purple),
-          ],
+        child: GridView.builder(
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: widget.emojis.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: const BoxDecoration(
+                  color: Color(0x55000000),
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        widget.emojis[index],
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      Text(
+                        widget.hobbies[index],
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
